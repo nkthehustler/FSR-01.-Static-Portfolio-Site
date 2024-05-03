7b9a8f24507a17c783c8276025ac9660a5a82f85
Nitin Kumar <66864065+nkthehustler@users.noreply.github.com>
2024-05-03 22:46:34 +0530

    changed sheets integration method

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
+// About Skills, Experience and Education Tab Script
+
 var tablinks = document.getElementsByClassName("tab-links")
 var tabcontents = document.getElementsByClassName("tab-content")
 
 
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+function opentab(tabname) {
+   for (tablink of tablinks) {
+      tablink.classList.remove("active-link");
+   }
+   for (tabcontent of tabcontents) {
+      tabcontent.classList.remove("active-tab");
+   }
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tabname).classList.add("active-tab");
 }
 
+// Hamburger Menu Script
 
 var sidemenu = document.getElementById("sidemenu");
 
-function openmenu(){
-    sidemenu.style.right = "0";
+function openmenu() {
+   sidemenu.style.right = "0";
 }
 
-function closemenu(){
-    sidemenu.style.right = "-200px";
+function closemenu() {
+   sidemenu.style.right = "-200px";
 }
 
+// form script
+
+const scriptURL = 'PASTE YOUR URL HERE'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+   e.preventDefault()
+   fetch(scriptURL, {
+         method: 'POST',
+         body: new FormData(form)
+      })
+      .then(response => {
+         msg.innerHTML = "Message Sent!"
+         setTimeout(function () {
+            msg.innerHTML = '';
+         }, 5000);
+         form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+})
